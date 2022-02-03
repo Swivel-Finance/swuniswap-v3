d7d50e6e239d0851135f707724d1bacb4e63d597
Janko <simonovic86@gmail.com>
2022-02-03 21:03:33 +0100

    add more env vars

--- constants/__init__.py
+++ constants/__init__.py
@@ -11,9 +11,8 @@ AMOUNT = float(os.getenv("AMOUNT", 50000))  # The amount of n/zcTokens to use ma
 # Assumes a 50/50 range distribution around current market rates. If distributions are uneven, more n/zcTokens are required
 UPPER_RATE = float(os.getenv("UPPER_RATE", 5.5))  # The highest rate at which to quote
 LOWER_RATE = float(os.getenv("LOWER_RATE", 2.75))  # The lowest rate at which to quote
-NUM_TICKS = int(3)  # The number of liquidity ticks to split your amount into (Per side + 1 at market price)
-COMPOUND_RATE_LEAN = float(
-    1)  # How much your quote should change when Compound’s rate varies (e.g. 1 = 1:1 change in price)
-EXPIRY_LENGTH = float(600)  # How often orders should be refreshed (in seconds)
+NUM_TICKS = int(os.getenv("NUM_TICKS", 3))  # The number of liquidity ticks to split your amount into (Per side + 1 at market price)
+COMPOUND_RATE_LEAN = float(os.getenv("COMPOUND_RATE_LEAN", 1))  # How much your quote should change when Compound’s rate varies (e.g. 1 = 1:1 change in price)
+EXPIRY_LENGTH = float(os.getenv("EXPIRY_LENGTH", 600))  # How often orders should be refreshed (in seconds)
 
 PUBLIC_KEY = "0xb42Af00422f53e09FC97C3Af041Ddd0B19E936A5"
